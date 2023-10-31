3eeed623bbd981b16622589b98f85cc43b695ddf
Gribanov_German <germangribanov@mail.ru>
2023-10-31 14:33:45 +0400

    feat: add env variable

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.MAIN_URL': JSON.stringify(
+      process.env.MAIN_URL || 'https://pokeapi.co/api/v2/pokemon'
+    ),
+  },
 });
